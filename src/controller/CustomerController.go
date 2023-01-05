47a7816dfea109e87a1ae20212469e9cfe5d14c5
ousta <ousta@aefis.com>
2023-01-06 01:50:43 +0300

    Deleting a customer through a /customers/{id} path

--- src/controller/CustomerController.go
+++ src/controller/CustomerController.go
@@ -17,9 +17,7 @@ func GetCustomers(responseWriter http.ResponseWriter, request *http.Request) {
 
 func GetCustomer(responseWriter http.ResponseWriter, request *http.Request) {
 	responseWriter.Header().Set("Content-Type", "application/json")
-
-	item := mux.Vars(request)
-	headerId, _ := strconv.ParseInt(item["id"], 10, 64)
+	headerId := getIdFromRequestHeader(request)
 	customer := service.GetAllCustomer(headerId)
 
 	if customer.Id == headerId {
@@ -35,9 +33,25 @@ func CreateCustomer(responseWriter http.ResponseWriter, request *http.Request) {
 }
 
 func DeleteCustomer(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "application/json")
+	headerId := getIdFromRequestHeader(request)
+	customer := service.GetAllCustomer(headerId)
 
+	if customer.Id == headerId {
+		service.DeleteCustomer(headerId)
+		responseWriter.WriteHeader(http.StatusOK)
+		json.NewEncoder(responseWriter).Encode(service.GetAllCustomers())
+	} else {
+		responseWriter.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func UpdateCustomer(responseWriter http.ResponseWriter, request *http.Request) {
 
+}
+
+func getIdFromRequestHeader(request *http.Request) int64 {
+	item := mux.Vars(request)
+	headerId, _ := strconv.ParseInt(item["id"], 10, 64)
+	return headerId
 }
