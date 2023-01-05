35f0a821c79772083e54afddc83398ff9e944495
ousta <ousta@aefis.com>
2023-01-06 02:15:42 +0300

    code refactoring

--- src/controller/CustomerController.go
+++ src/controller/CustomerController.go
@@ -33,23 +33,13 @@ func GetCustomer(responseWriter http.ResponseWriter, request *http.Request) {
 
 func CreateCustomer(responseWriter http.ResponseWriter, request *http.Request) {
 	responseWriter.Header().Set("Content-Type", "application/json")
-	body, err := ioutil.ReadAll(request.Body)
-	if err != nil {
-		fmt.Print(err)
-		responseWriter.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	var customer model.Customer
-	err = json.Unmarshal(body, &customer)
-	if err != nil {
-		fmt.Print(err)
-		responseWriter.WriteHeader(http.StatusBadRequest)
-		return
-	}
 
-	customer = service.CreateCustomer(customer)
-	responseWriter.WriteHeader(http.StatusCreated)
-	json.NewEncoder(responseWriter).Encode(customer)
+	var customer model.Customer
+	if validateRequestAndParseBody(responseWriter, request, &customer) {
+		customer = service.CreateCustomer(customer)
+		responseWriter.WriteHeader(http.StatusCreated)
+		json.NewEncoder(responseWriter).Encode(customer)
+	}
 }
 
 func DeleteCustomer(responseWriter http.ResponseWriter, request *http.Request) {
@@ -74,4 +64,20 @@ func getIdFromRequestHeader(request *http.Request) int64 {
 	item := mux.Vars(request)
 	headerId, _ := strconv.ParseInt(item["id"], 10, 64)
 	return headerId
+}
+
+func validateRequestAndParseBody(responseWriter http.ResponseWriter, request *http.Request, customerRef *model.Customer) bool {
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		fmt.Print(err)
+		responseWriter.WriteHeader(http.StatusBadRequest)
+		return false
+	}
+	err = json.Unmarshal(body, customerRef)
+	if err != nil {
+		fmt.Print(err)
+		responseWriter.WriteHeader(http.StatusBadRequest)
+		return false
+	}
+	return true
 }
